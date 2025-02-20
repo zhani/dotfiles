772684278939779ffdfff2c930b00d40fe7eb386
Jani Hautakangas <jani@kodegood.com>
2025-02-20 17:12:33 +0200

    Add blink-cmp config

---
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,97 @@
+-- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/blink-cmp.lua
+-- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/blink-cmp.lua
+
+-- HACK: blink.cmp updates | Remove LuaSnip | Emoji and Dictionary Sources | Fix Jump Autosave Issue
+-- https://youtu.be/JrgfpWap_Pg
+
+-- completion plugin with support for LSPs and external sources that updates
+-- on every keystroke with minimal overhead
+
+-- https://www.lazyvim.org/extras/coding/blink
+-- https://github.com/saghen/blink.cmp
+-- Documentation site: https://cmp.saghen.dev/
+
+-- NOTE: Specify the trigger character(s) used for luasnip
+local trigger_text = ";"
+
+return {
+  "saghen/blink.cmp",
+  enabled = true,
+
+  opts = function(_, opts)
+    -- I noticed that telescope was extremeley slow and taking too long to open,
+    -- assumed related to blink, so disabled blink and in fact it was related
+    -- :lua print(vim.bo[0].filetype)
+    -- So I'm disabling blink.cmp for Telescope
+    opts.enabled = function()
+      -- Get the current buffer's filetype
+      local filetype = vim.bo[0].filetype
+      -- Disable for Telescope buffers
+      if filetype == "TelescopePrompt" or filetype == "minifiles" or filetype == "snacks_picker_input" then
+        return false
+      end
+      return true
+    end
+
+    opts.cmdline = {
+      -- command line completion, thanks to dpetka2001 in reddit
+      -- https://www.reddit.com/r/neovim/comments/1hjjf21/comment/m37fe4d/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
+        if type == ":" then
+          return { "cmdline" }
+        end
+        return {}
+      end,
+    }
+
+    opts.completion = {
+      --   keyword = {
+      --     -- 'prefix' will fuzzy match on the text before the cursor
+      --     -- 'full' will fuzzy match on the text before *and* after the cursor
+      --     -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+      --     range = "full",
+      --   },
+      menu = {
+        border = "single",
+      },
+      documentation = {
+        auto_show = true,
+        window = {
+          border = "single",
+        },
+      },
+      -- Displays a preview of the selected item on the current line
+      ghost_text = {
+        enabled = true,
+      },
+    }
+
+    -- The default preset used by lazyvim accepts completions with enter
+    -- I don't like using enter because if on markdown and typing
+    -- something, but you want to go to the line below, if you press enter,
+    -- the completion will be accepted
+    -- https://cmp.saghen.dev/configuration/keymap.html#default
+    opts.keymap = {
+      preset = "default",
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+
+      ["<S-k>"] = { "scroll_documentation_up", "fallback" },
+      ["<S-j>"] = { "scroll_documentation_down", "fallback" },
+
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+    }
+
+    return opts
+  end,
+}
