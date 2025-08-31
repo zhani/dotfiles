28793949b9eecc9453817cc8deedf705c11050bf
Jani Hautakangas <jani@kodegood.com>
2025-08-31 12:48:51 +0300

    nvim: use OSC52 only on remote hosts

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,26 @@
 
 -- Disable LazyVim auto format
 vim.g.autoformat = false
+
+-- SSH-only: use Neovim's built-in OSC52 provider and make y/yy go to system clipboard.
+if vim.env.SSH_TTY or vim.env.SSH_CONNECTION or vim.env.SSH_CLIENT then
+  local o = vim.opt
+  local g = vim.g
+  local osc52 = require("vim.ui.clipboard.osc52")
+
+  -- Make default yanks target the system clipboard (+)
+  o.clipboard = "unnamedplus"
+
+  -- IMPORTANT: bind the factory functions to "+" and "*"
+  g.clipboard = {
+    name = "OSC 52",
+    copy  = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = osc52.paste("+"),
+      ["*"] = osc52.paste("*"),
+    },
+  }
+end
